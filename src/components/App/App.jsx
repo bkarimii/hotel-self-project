4b94ddc7ee4bd9b42eba20321ffd598874326bd2
karimi <behrouzkarimi1993@gmail.com>
2024-03-04 22:33:26 +0000

    deck component jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
 import AppHeader from "../AppHeader/AppHeader";
+import Deck from "../Deck/Deck";
 
 const App = () => (
   <div className="app">
     <AppHeader />
+    <Deck />
     <Bookings />
   </div>
 );

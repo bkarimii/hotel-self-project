647633c5983b7b37f76e549e85f538d4a3b49b18
karimi <behrouzkarimi1993@gmail.com>
2024-03-04 23:14:14 +0000

    Footer created , code was wrote , test file created

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,24 @@ import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
 import AppHeader from "../AppHeader/AppHeader";
 import Deck from "../Deck/Deck";
+import Footer from "../Footer/Footer";
 
-const App = () => (
-  <div className="app">
-    <AppHeader />
-    <Deck />
-    <Bookings />
-  </div>
-);
+const App = () => {
+  const footerArray = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789",
+  ];
+
+  return (
+    <div className="app">
+      <AppHeader />
+      <Deck />
+      <Bookings />
+      <Footer data={footerArray} />
+    </div>
+  );
+};
 
 export default App;
 // test comment

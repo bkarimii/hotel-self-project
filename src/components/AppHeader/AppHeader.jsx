f95f299edc564aff400a91ed1f5c44a065180093
karimi <behrouzkarimi1993@gmail.com>
2024-03-25 19:06:48 +0000

    minor adjustments

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,9 +1,10 @@
 import Logo from "@/assets/spa-logo.png";
 import "./AppHeader.scss";
+
 const AppHeader = () => (
   <header className="app__header">
     <h1 className="app__heading">CYF Hotel</h1>
-    <img src="./src/assets/hotel-logo.png" />
+    {/* <img src="./src/assets/hotel-logo.png" /> */}
   </header>
 
   // what goes in here? there is no content in the AppHeader component
--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -5,8 +5,10 @@ import cardsData from "@/data/fakeCards.json";
 const Deck = () => {
   // you will need to map over the cardsData array and render a Card component for each card object
   // how will you pass props to the Card component?
-  const deckCards = cardsData.map((card,index) => (
-    <li key={index}>{<Card title={card.title} url={card.url} image={card.image} />}</li>
+  const deckCards = cardsData.map((card, index) => (
+    <li key={index}>
+      <Card title={card.title} url={card.url} image={card.image} />
+    </li>
   ));
   return (
     <>
